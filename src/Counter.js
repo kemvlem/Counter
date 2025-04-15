dc82d39fdaf927358ed42b08ead1be6dbff05c8d
Kem <67571861+kemvlem@users.noreply.github.com>
2025-04-14 21:37:56 -0400

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,68 +1,15 @@
 import React from 'react';
 
-class Counter extends React.Component{
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      doubleCount: false,
-  };
-}
+export function counter(){
+  const [count, setCount] = React.useState(0)
 
- increment = () => {
-  if (this.state.count < 20)
-   this.setState({
-    count: this.state.count + 1,
-  });
-if(this.state.doubleCount && this.state.count < 19){
-  this.setState({
-    count: this.state.count + 2
-  })
- }
-};
-
-decrement = () => {
-if (this.state.count > 1)
-  this.setState({
-    count: this.state.count - 1,
-  });
-}
-
-toggle = () => {
- if (this.state.button === 'Single Increment' && this.state.count < 18) {
-   this.setState({
-     count: this.state.count + 1,
-     button: 'Double Increment'
-   })
- } else if (this.state.button === 'Double Increment' && this.state.count < 18) {
-   this.setState({
-     count: this.state.count + 2,
-     button: 'Single Increment'
-   })
- } else {
-    return
-  }
-}
-
-render(){
-  return(
-    <div className="container">
-      <div className="navbar">Counter</div>
-      <div className="counter">
-        <h1>{this.state.count}</h1>
-        <button type="button" onClick={this.increment}>
-          Increment
-          </button>
-          <button type="button" onClick={this.decrement}>
-            Decrement
-          </button>
-          <button type= "button" onClick={this.clear}>
-            clear
-          </button>
-      </div>
+  return (
+    <div>
+    <button onClick={()=> setCount((prevState)=> prevState - 1)}>-</button>
+     {count}
+    <button onClick={()=> setCount((prevState)=> prevState + 1)}>+</button>
     </div>
-  );
- }
+  )
 }
 
 export default Counter;
